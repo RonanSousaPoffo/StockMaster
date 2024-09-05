15a63608c0aea0316605f5e2c4e91fee2a144510
Ronankill <poffo2014@gmail.com>
2024-09-05 18:47:22 -0300

    Adicionada na tela de controle de estrada e saída categoria

--- frontend/src/components/StockMovement.js
+++ frontend/src/components/StockMovement.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { db } from '../firebaseConfig';
 import { collection, addDoc, getDocs, query, orderBy, where, doc, updateDoc } from 'firebase/firestore';
 import './StockMovement.css';
-import { getAuth } from 'firebase/auth'; // Importar getAuth para obter o usuário autenticado
 
 const StockMovement = () => {
   const [item, setItem] = useState('');
@@ -28,8 +27,13 @@ const StockMovement = () => {
         setItems(data);
         setFilteredItems(data);
 
-        const uniqueCategories = Array.from(new Set(data.map(item => item.category).filter(cat => cat)));
-        setCategories(uniqueCategories);
+        const categoryDocs = await getDocs(collection(db, 'categories'));
+        const categoryData = categoryDocs.docs.reduce((acc, doc) => {
+          acc[doc.id] = doc.data().name;
+          return acc;
+        }, {});
+
+        setCategories(categoryData);
       } catch (error) {
         console.error('Erro ao carregar itens:', error);
       }
@@ -37,6 +41,15 @@ const StockMovement = () => {
 
     fetchItems();
   }, []);
+
+  useEffect(() => {
+    if (filters.category) {
+      const categoryItems = items.filter(item => item.categoryID === filters.category);
+      setFilteredItems(categoryItems);
+    } else {
+      setFilteredItems(items);
+    }
+  }, [filters.category, items]);
 
   useEffect(() => {
     const fetchMovements = async () => {
@@ -53,12 +66,27 @@ const StockMovement = () => {
           q = query(q, where('timestamp', '<=', new Date(filters.endDate)));
         }
         if (filters.category) {
-          const categoryItems = items.filter(item => item.category === filters.category).map(item => item.name);
+          const categoryItems = items.filter(item => item.categoryID === filters.category).map(item => item.name);
           q = query(q, where('item', 'in', categoryItems));
         }
 
         const querySnapshot = await getDocs(q);
-        const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        const data = await Promise.all(querySnapshot.docs.map(async doc => {
+          const movement = { id: doc.id, ...doc.data() };
+
+          // Fetch the category name using itemID
+          const itemDoc = await getDocs(query(collection(db, 'items'), where('name', '==', movement.item)));
+          if (!itemDoc.empty) {
+            const itemData = itemDoc.docs[0].data();
+            const categoryID = itemData.categoryID;
+            const categoryDoc = await getDocs(query(collection(db, 'categories'), where('__name__', '==', categoryID)));
+            movement.category = categoryDoc.docs[0]?.data()?.name || 'Categoria não encontrada';
+          } else {
+            movement.category = 'Categoria não encontrada';
+          }
+
+          return movement;
+        }));
         setMovements(data);
       } catch (error) {
         console.error('Erro ao carregar movimentações:', error);
@@ -74,45 +102,34 @@ const StockMovement = () => {
       if (isNaN(quantity) || quantity <= 0) {
         throw new Error("Quantidade inválida");
       }
-       // Obter o usuário autenticado
-       const auth = getAuth();
-       const user = auth.currentUser;
 
-      // Adiciona movimentação
-      await addDoc(collection(db, 'movements'), {
-        item,
-        quantity,
-        type,
-        timestamp: new Date(),
-        movimentBy: user ? user.email : 'Usuário não autenticado',
-      });
-
-      // Atualiza a quantidade do item
       const itemDoc = await getDocs(query(collection(db, 'items'), where('name', '==', item)));
       const itemData = itemDoc.docs[0]?.data();
       if (itemData) {
+        const itemID = itemDoc.docs[0].id;
         const currentQuantity = itemData.quantity || 0;
         const newQuantity = type === 'entrada' ? currentQuantity + quantity : currentQuantity - quantity;
-        await updateDoc(doc(db, 'items', itemDoc.docs[0].id), { quantity: newQuantity });
+
+        await addDoc(collection(db, 'movements'), {
+          item,
+          itemID,
+          quantity,
+          type,
+          timestamp: new Date()
+        });
+
+        await updateDoc(doc(db, 'items', itemID), { quantity: newQuantity });
         
-        // Atualiza a lista de movimentações e limpa os campos
         setItem('');
         setQuantity(0);
         setType('entrada');
-        setMovements([{ item, quantity, type, timestamp: new Date() }, ...movements]);
+        setMovements([{ item, itemID, quantity, type, timestamp: new Date(), category: categories[itemData.categoryID] }, ...movements]);
       } else {
-        // Item não encontrado, pode ser necessário adicionar uma lógica para lidar com isso
         console.error('Item não encontrado na atualização');
       }
     } catch (error) {
       console.error("Erro ao adicionar movimentação:", error.message);
     }
-  };
-
-  const handleSearch = (e) => {
-    const query = e.target.value.toLowerCase();
-    const filtered = items.filter(item => item.name.toLowerCase().includes(query));
-    setFilteredItems(filtered);
   };
 
   const handleFilterChange = (e) => {
@@ -121,16 +138,6 @@ const StockMovement = () => {
       ...filters,
       [name]: value
     });
-  };
-
-  const handleItemChange = (e) => {
-    const selectedItem = e.target.value;
-    setItem(selectedItem);
-    const itemCategory = items.find(i => i.name === selectedItem)?.category || '';
-    setFilters(prevFilters => ({
-      ...prevFilters,
-      category: itemCategory
-    }));
   };
 
   const handleClearFilters = () => {
@@ -144,27 +151,27 @@ const StockMovement = () => {
     setFilteredItems(items);
   };
 
-  const handleQuantityChange = (e) => {
-    const value = parseFloat(e.target.value);
-    if (!isNaN(value)) {
-      setQuantity(value);
-    } else {
-      setQuantity(0);
-    }
-  };
-
   return (
     <div className="stock-movement-container">
       <h1>Controle de Entrada e Saída de Itens</h1>
       <form onSubmit={handleAddMovement} className="movement-form">
-        <input
-          type="text"
-          placeholder="Pesquisar Item"
-          onChange={handleSearch}
-        />
+        {/* Campo de Categoria */}
+        <select
+          name="category"
+          value={filters.category}
+          onChange={handleFilterChange}
+          required
+        >
+          <option value="">Selecione uma Categoria</option>
+          {Object.keys(categories).map(categoryID => (
+            <option key={categoryID} value={categoryID}>{categories[categoryID]}</option>
+          ))}
+        </select>
+
+        {/* Campo de Item */}
         <select
           value={item}
-          onChange={handleItemChange}
+          onChange={(e) => setItem(e.target.value)}
           required
         >
           <option value="">Selecione um Item</option>
@@ -172,11 +179,12 @@ const StockMovement = () => {
             <option key={i.id} value={i.name}>{i.name}</option>
           ))}
         </select>
+
         <input
           type="number"
           placeholder="Quantidade"
           value={quantity}
-          onChange={handleQuantityChange}
+          onChange={(e) => setQuantity(parseFloat(e.target.value) || 0)}
           required
         />
         <select value={type} onChange={(e) => setType(e.target.value)} required>
@@ -185,7 +193,7 @@ const StockMovement = () => {
         </select>
         <button type="submit">Adicionar Movimentação</button>
       </form>
-      
+
       <div className="filter-container">
         <h2>Filtros</h2>
         <input
@@ -195,11 +203,10 @@ const StockMovement = () => {
           value={filters.itemName}
           onChange={handleFilterChange}
         />
-         <label>Data Inicial:</label>
+        <label>Data Inicial:</label>
         <input
           type="date"
           name="startDate"
-          placeholder="Data de Início"
           value={filters.startDate}
           onChange={handleFilterChange}
         />
@@ -207,37 +214,23 @@ const StockMovement = () => {
         <input
           type="date"
           name="endDate"
-          placeholder="Data de Fim"
           value={filters.endDate}
           onChange={handleFilterChange}
         />
-        <select
-          name="category"
-          className="category-select"
-          value={filters.category}
-          onChange={handleFilterChange}
-        >
-       <option value="">Categoria</option>
-       {categories.map((cat, index) => (
-       <option key={index} value={cat}>{cat}</option>
-  ))}
-</select>
-        <div className="Limpa-Filtros">
         <button onClick={handleClearFilters}>Limpar Filtros</button>
-        </div>
       </div>
-      
+
       <div className="movement-list">
         <h2>Histórico de Movimentações</h2>
         <ul>
           {movements.length > 0 ? (
             movements.map(movement => (
               <li key={movement.id}>
-                <strong>Item:</strong> <span>{movement.item}</span>     
-                <strong>Tipo:</strong> <span>{movement.type === 'entrada' ? 'Entrada' : 'Saída'}</span>            
-                <strong>Quantidade:</strong> <span>{isNaN(movement.quantity) ? 'Quantidade inválida' : `${movement.quantity} unidades`}  </span>            
-                <strong>Data/Hora:</strong> <span>{new Date(movement.timestamp.seconds * 1000).toLocaleString()}  </span>    
-                <strong>Responsável:</strong> <span>{movement.movimentBy}</span>                              
+                <strong>Item:</strong> {movement.item}
+                <strong>Categoria:</strong> {movement.category}
+                <strong>Quantidade:</strong> {movement.quantity}
+                <strong>Tipo:</strong> {movement.type === 'entrada' ? 'Entrada' : 'Saída'}
+                <strong>Data/Hora:</strong> {new Date(movement.timestamp.seconds * 1000).toLocaleString()}
               </li>
             ))
           ) : (
